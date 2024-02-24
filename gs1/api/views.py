6555265590c89b04fd381c335b580d4e28a6f7bf
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-02-24 16:09:48 +0530

    updated

--- gs1/api/views.py
+++ gs1/api/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 #  Model Object -Single Student Data
 def student_detail(request,pk):
     stu = Student.objects.get(id=pk)  # pylint: disable=no-member
     serializer = StudentSerializer(stu)
     json_data = JSONRenderer().render(serializer.data)
     return HttpResponse(json_data,content_type = 'application/json')
-
+    # return JsonResponse(serializer.data)
 
 # Query set - all student data
 def student_list(request):
     stu = Student.objects.all() # pylint: disable=no-member
     serializer = StudentSerializer(stu, many=True)
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data,content_type = 'application/json')
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data,content_type = 'application/json')
+    return JsonResponse(serializer.data,safe=False)
