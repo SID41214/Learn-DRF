67b736399a5d56be259fbbadc7bd4fb5ef0c2765
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-03-05 15:24:26 +0530

    updated

--- gs11/api/views.py
+++ gs11/api/views.py
@@ -1,27 +1,64 @@
+from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveAPIView,UpdateAPIView,ListCreateAPIView,DestroyAPIView,RetrieveUpdateAPIView,RetrieveDestroyAPIView,RetrieveUpdateDestroyAPIView
 from .models import Student
 from .serializers import StudentSerializer
-from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveAPIView,UpdateAPIView,ListCreateAPIView
 
 
-class StudentList(ListAPIView):
-    queryset = Student.objects.all()
-    serializer_class=StudentSerializer
-
-
-class StudentCreate(CreateAPIView):
-    queryset = Student.objects.all()
-    serializer_class=StudentSerializer
-
-class StudentRetrieve(RetrieveAPIView):
+class StudentRUD(RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class=StudentSerializer
     
-
-class StudentUpdate(UpdateAPIView):
-    queryset = Student.objects.all()
-    serializer_class=StudentSerializer
 
 class StudentLC(ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class=StudentSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class StudentList(ListAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class=StudentSerializer
+
+
+# class StudentCreate(CreateAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class=StudentSerializer
+
+# class StudentRetrieve(RetrieveAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class=StudentSerializer
     
+
+# class StudentUpdate(UpdateAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class=StudentSerializer
+
+# class StudentLC(ListCreateAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class=StudentSerializer
+
+# class StudentDestroy(DestroyAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class=StudentSerializer
+
+# class StudentRU(RetrieveUpdateAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class=StudentSerializer
+
+# class StudentRD(RetrieveDestroyAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class=StudentSerializer
+
+# class StudentRUD(RetrieveUpdateDestroyAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class=StudentSerializer
+
--- gs11/gs11/urls.py
+++ gs11/gs11/urls.py
@@ -4,13 +4,28 @@ from django.urls import path
 from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/',views.StudentList.as_view()),
+    # path('studentapi/',views.StudentList.as_view()),
     # path('studentapi/',views.StudentCreate.as_view()),
+    # path('studentapi/',views.StudentLC.as_view()),
+   
+   
+   
+    path('studentapi/',views.StudentLC.as_view()),
+    
+    
+    
 
 
 
     # path('studentapi/<int:pk>',views.StudentRetrieve.as_view()),
-    path('studentapi/<int:pk>',views.StudentUpdate.as_view()),
+    # path('studentapi/<int:pk>',views.StudentUpdate.as_view()),
+    # path('studentapi/<int:pk>',views.StudentDestroy.as_view()),
+    # path('studentapi/<int:pk>',views.StudentRU.as_view()),
+    # path('studentapi/<int:pk>',views.StudentRD.as_view()),
+    # path('studentapi/<int:pk>',views.StudentRUD.as_view()),
+
+
+    path('studentapi/<int:pk>',views.StudentRUD.as_view()),
 
     
    
